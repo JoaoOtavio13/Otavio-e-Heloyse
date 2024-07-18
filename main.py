3bdd445fc9075ac1b1f4b5fa63115f8a6d7f7874
Helo8754 <heloyse.maria@escolar.ifrn.edu.br>
2024-07-18 08:11:07 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+class Pessoa:
+  def __init__(self, nomep, cpfp, enderecop):
+    self.__nome = nomep
+    self.__cpf = cpfp
+    self.__endereco = enderecop
+
+  def getAtributos(self):
+    print('> Nome: {}\n> CPF>: {}\n> endereço:{}'.format(self.__nome, self.__cpf, self.__endereco))
+
+
+  def Falar(self):
+    print('olá!')
+
